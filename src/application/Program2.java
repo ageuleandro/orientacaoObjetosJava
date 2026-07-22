5358fd6bedb708435e37e987b56f4218f8adac24
Ageu Leandro <leandro.junior7@gmail.com>
2026-07-22 11:25:40 -0300

    Adicionado Login

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -2,7 +2,7 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 import entities.Product;
-
+//Criado o login
 public class Program2 {
 
 	public static void main(String[] args) {
