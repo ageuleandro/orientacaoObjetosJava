22c4348258bb078f9ef5beb229814fb6d11ed1bf
Ageu Leandro <leandro.junior7@gmail.com>
2026-07-22 10:46:18 -0300

    Retirado Locale de Program3

--- src/application/Program3.java
+++ src/application/Program3.java
@@ -1,5 +1,4 @@
 package application;
-import java.util.Locale;
 import java.util.Scanner;
 import entities.Employee;
 
@@ -7,7 +6,6 @@ public class Program3 {
 
 	public static void main(String[] args) {
 		
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		Employee empregado = new Employee();
 		
